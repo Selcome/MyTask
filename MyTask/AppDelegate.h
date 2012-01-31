33a5500629e117d9e0d5c6866dab11f8a732d826
Marshal Wu <marshal.wu@gmail.com>
2012-01-31 11:44:40 +0800

    Initial Commit

---
+++ MyTask/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyTask
+//
+//  Created by Marshal Wu on 12-1-31.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
