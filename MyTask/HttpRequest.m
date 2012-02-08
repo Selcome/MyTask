d113d876fd5b610ddd0ea5c44b18133bcde398d8
marcuswang <marcus.jun@qq.com>
2012-02-08 12:37:38 +0800

    添加网络分组

---
+++ MyTask/HttpRequest.m
@@ -0,0 +1,13 @@
+//
+//  HttpRequest.m
+//  MyTask
+//
+//  Created by marcus wang on 12-2-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "HttpRequest.h"
+
+@implementation HttpRequest
+
+@end
