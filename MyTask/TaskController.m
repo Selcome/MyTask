7273ed3d51fc2fad369aa4017c1562d7b0a49e79
marcuswang <marcus.jun@qq.com>
2012-02-19 22:04:10 +0800

    commit

---
+++ MyTask/TaskController.m
@@ -0,0 +1,113 @@
+//
+//  TaskController.m
+//  MyTask
+//
+//  Created by Marshal Wu on 12-2-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "TaskController.h"
+#import "TaskDetailTableView.h"
+#import "DataManager.h"
+
+@interface TaskController ()
+-(void)setCurrentView:(UIView *) theView;
+@end
+
+@implementation TaskController{
+    __weak UIView *currentSubView;
+    NSDictionary *activity;
+}
+
+-(void)setCurrentView:(UIView *) theView{
+    currentSubView=theView;
+    [subViewContainer addSubview:theView];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil id:(NSString *) theId
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        activity=[[DataManager shareInstance] getActivity:theId];
+        self.navigationItem.title=[activity objectForKey:@"title"];
+    }
+    return self;
+}
+
+- (IBAction)switchSubView:(id)sender {
+    UISegmentedControl *segment=(UISegmentedControl *)sender;
+    switch ([segment selectedSegmentIndex]) {
+        case 0:
+            if(currentSubView!=taskDetailView){
+                [currentSubView removeFromSuperview];
+                [self setCurrentView:taskDetailView];
+                self.navigationItem.rightBarButtonItem=anotherButton;
+            }
+            break;
+        case 1:
+            if(currentSubView!=commentView){
+                [currentSubView removeFromSuperview];
+                [self setCurrentView:commentView];
+                self.navigationItem.rightBarButtonItem=nil;
+            }
+            break;
+        case 2:
+            if(currentSubView!=childTaskView){
+                [currentSubView removeFromSuperview];
+                [self setCurrentView:childTaskView];
+                self.navigationItem.rightBarButtonItem=nil;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    taskDetailView.activity=activity;
+    
+    [self setCurrentView:taskDetailView];
+    
+     anotherButton = [[UIBarButtonItem alloc] initWithTitle:@"完成" style:UIBarButtonItemStylePlain 
+                                                    target:self action:@selector(onClickSubmitButton:)];          
+    self.navigationItem.rightBarButtonItem = anotherButton; 
+}
+
+- (void)viewDidUnload
+{
+    subViewContainer = nil;
+    taskDetailView = nil;
+    commentView = nil;
+    childTaskView = nil;
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+-(void)onClickSubmitButton:(id)sender
+{
+    [[DataManager shareInstance] deleteActivity:activity];
+     [self.navigationController popViewControllerAnimated:YES];
+}
+@end
